ba9d46cb863575bdcc000f7d5f5b8db2566fc09e
Idongesit Ataide <idongataide@gmail.com>
2025-09-12 19:12:15 +0100

    update

--- about-us.php
+++ about-us.php
@@ -49,9 +49,7 @@
     <div class="bottom-shape">
         <img src="assets/img/section-top-shape-2.png" alt="shape-img">
     </div>
-    <div class="sun-shape">
-        <img src="assets/img/client/shape-2.png" alt="shape-img">
-    </div>
+    
     <div class="container">
         <div class="about-activities-wrapper style-2 mb-55">
             <div class="row g-4">               
@@ -66,24 +64,31 @@
                         <p class="mt-3 mt-md-0 wow fadeInUp" data-wow-delay=".5s">
                             If you’ve ever scrolled through Reddit homeschool threads late at night, you’ve probably seen it.
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay=".6s">
                             Post after post from homeschoolers admitting they feel isolated, parents worrying about “socialization,” and teens searching for friends who understand. The conversations are endless, but the solutions? They’re scattered, expensive, or nonexistent.
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay=".7s">
                             That’s the flaw of homeschooling that no one really wants to talk about: freedom comes at the cost of connection. Without community, the independence of homeschooling can turn into isolation, and isolation makes it harder to grow, feel confident, and harder to remember that you’re not the only one living this way.
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay=".8s">
                             <strong>Bridginality was created to change this.</strong>
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay=".9s">
                             We’re proud to say Bridginality is completely homeschooler-run and homeschooler-owned. No corporations, or outside adults telling us what our community should look like. Just us, the students, building something real together.
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay="1s">
                             Here, you’ll find peer-led clubs, leadership opportunities, and friendships that cross borders. From Model UN to Crochet & Fiber Arts to Women in STEM, our spaces are built by people who know what it’s like to be homeschooled, because we are!
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay="1.1s">
                             Learning should never feel lonely, irrespective of your situation, which is why we’ve created this community. A place where homeschoolers can belong, lead, and connect on our own terms.
                         </p>
+                        <br/>
                         <p class="wow fadeInUp" data-wow-delay="1.2s">
                             Homeschooling is about freedom, and that freedom should include friendship.
                         </p>
